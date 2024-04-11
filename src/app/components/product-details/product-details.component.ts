825c3f090b220302ca6e47ac4c0b75b64d22f3b3
Mitul <mitulgondaliya27@gmail.com>
2024-04-11 18:03:26 +0530

    productDetails

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    const gray = document.querySelector('.gray') as HTMLElement;
+    const red = document.querySelector('.red') as HTMLElement;
+
+    gray.addEventListener("click", () => {
+      gray.style.border = "2px solid black"
+      red.style.border = "none"
+    });
+
+    red.addEventListener("click", () => {
+      red.style.border = "2px solid black"
+      gray.style.border = "none"
+    });
+
+  }
 
 }
