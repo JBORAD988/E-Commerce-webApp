9947df8d1dd2ff65db56f05493aa8ef1e9059b68
Mitul <mitulgondaliya27@gmail.com>
2024-04-11 18:11:59 +0530

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
-  { path: '', component: ProductDetailsComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   {path: 'forgotpass', component: ForgotpassComponent},
@@ -27,6 +27,7 @@ const routes: Routes = [
   { path: 'wishlist', component: WishlistComponent },
   { path: 'account', component: AccountComponent },
   { path: 'cart/checkout', component: CheckoutComponent },
+  { path: 'product', component: ProductDetailsComponent },
 
 ];
 
